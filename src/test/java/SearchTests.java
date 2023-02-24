6f3cc262fa9175f0b6f5d87d9a807a92d9848bfd
Mikhail Lebedev <mhl.lbdvvv@gmail.com>
2023-02-25 01:14:17 +0300

    first commit

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -4,11 +4,13 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SearchTests {
+class giSearchTests {
     @Test
     void successfulSearchTest() {
-        open("https://www.google.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://selenide.org"));
+        System.out.print("test  test  test  test  test  test  test  test  test  test  test  test  test  test  test    ");
+//        open("https://www.google.com/");
+//        $("[name=q]").setValue("selenide").pressEnter();
+//        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
 }
+
