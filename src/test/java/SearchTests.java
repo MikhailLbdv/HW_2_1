c6e7aeb5c1c3ac49dcab50722589dc04004d6fdd
MikhailLbdv <125909446+MikhailLbdv@users.noreply.github.com>
2023-02-26 23:48:15 +0300

    Update SearchTests.java

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,16 +1,7 @@
-import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 class giSearchTests {
-    @Test
     void successfulSearchTest() {
 System.out.print("merge conflict ");
-//        open("https://www.google.com/");
-//        $("[name=q]").setValue("selenide").pressEnter();
-//        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
     }
 }
 
